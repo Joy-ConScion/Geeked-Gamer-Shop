250b6c3648da12cf65a9d84983aa6006f444d713
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-22 15:07:48 -0500

    Add quick code per intelliJ recommendation to make them final

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -20,8 +20,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 public class CategoriesController {
-    private CategoryService categoryService;
-    private ProductService productService;
+    private final CategoryService categoryService;
+    private final ProductService productService;
 
 
     // create an Autowired constructor to inject the categoryService and productService
