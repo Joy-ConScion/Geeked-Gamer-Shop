79d0c393a9b3a73d3a3947d09664b543df69d2c9
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-23 15:30:40 -0500

    Add different skelatal methods

--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -14,12 +14,29 @@ public class ProfileService
         this.profileRepository = profileRepository;
     }
 
+    public Profile getById(int userId) {
+        return profileRepository.findById(userId)
+                .orElse(null);
+    }
+
     public Profile create(Profile profile)
     {
         return profileRepository.save(profile);
     }
 
+    public Profile update(int userId) {
+        if (!profileRepository.existsById(userId)) {return null;}
+        profile.setCategoryId(userId);
+        return profileRepository.save(userId);
 
+    }
 
+    public boolean delete(int userId) {
+        if (profileRepository.existsById(userId)) {
+            profileRepository.deleteById(userId);
+            return true;
+        }
+        return false;
+    }
 
 }
