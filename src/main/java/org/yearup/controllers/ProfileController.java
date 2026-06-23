6c349e35e22a0203359088052358ba2d05855f86
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-23 14:50:03 -0500

    Code check-in > working through profileController and realized I need to populate profileService, switching to PS that before resuming PC

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,4 +1,29 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.models.Profile;
+import org.yearup.service.ProfileService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("profile")
 public class ProfileController {
+    private final ProfileService profileService;
+
+    @Autowired
+    public ProfileController(ProfileService profileService){
+        this.profileService = profileService;
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("permitAll()")
+    public Profile getUserId(@PathVariable int userId){
+
+        Profile profile = profileService.getUserId
+
+
+    }
+
 }
--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -18,4 +18,8 @@ public class ProfileService
     {
         return profileRepository.save(profile);
     }
+
+
+
+
 }
