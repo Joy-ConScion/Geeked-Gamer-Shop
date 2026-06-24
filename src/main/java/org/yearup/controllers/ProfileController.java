4a4759247437ddfb9bd538e428545185f2c122e8
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-24 09:55:39 -0500

    Add code to ProfileController to flesh it out > Untested

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,8 +1,12 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.models.Product;
 import org.yearup.models.Profile;
 import org.yearup.service.ProfileService;
 
@@ -20,10 +24,35 @@ public class ProfileController {
     @GetMapping("/{id}")
     @PreAuthorize("permitAll()")
     public Profile getUserId(@PathVariable int userId){
-
-        Profile profile = profileService.getUserId
-
-
+        Profile profile = profileService.getById(userId);
+        if (profile==null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return profile;
     }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Profile> create(@RequestBody Profile profile){
+        Profile saved = profileService.create((profile));
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Profile update(@PathVariable int userId, @RequestBody Profile profile){
+        if (profileService.getById(userId) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return profileService.update(userId, profile);
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<Void> delete(@PathVariable int userId){
+        if (profileService.getById(userId) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        profileService.delete(userId);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
