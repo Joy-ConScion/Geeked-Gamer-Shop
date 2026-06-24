264221ce375054b36fe9cd97264e6ddfc51622c8
Joy-ConScion <chris.torres2001@outlook.com>
2026-06-24 10:16:35 -0500

    Code check-in. Running into 405 & 500

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -8,21 +8,26 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.models.Product;
 import org.yearup.models.Profile;
+import org.yearup.models.User;
 import org.yearup.service.ProfileService;
+import org.yearup.service.UserService;
 
 @CrossOrigin
 @RestController
-@RequestMapping("profile")
+@RequestMapping("/profile")
+@PreAuthorize("isAuthenticated()")
 public class ProfileController {
     private final ProfileService profileService;
+    private final UserService userService;
 
     @Autowired
-    public ProfileController(ProfileService profileService){
+    public ProfileController(ProfileService profileService, UserService userService){
         this.profileService = profileService;
+        this.userService = userService;
     }
 
-    @GetMapping("/{id}")
-    @PreAuthorize("permitAll()")
+    @GetMapping
+//    @PreAuthorize("permitAll()")
     public Profile getUserId(@PathVariable int userId){
         Profile profile = profileService.getById(userId);
         if (profile==null)
@@ -31,22 +36,22 @@ public class ProfileController {
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Profile> create(@RequestBody Profile profile){
         Profile saved = profileService.create((profile));
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
-    @PutMapping("{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public Profile update(@PathVariable int userId, @RequestBody Profile profile){
         if (profileService.getById(userId) == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return profileService.update(userId, profile);
     }
 
-    @DeleteMapping("{id}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable int userId){
         if (profileService.getById(userId) == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
